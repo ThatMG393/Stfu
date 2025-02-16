ddcff01d77fd7e094cef771afdd87e7cec371f74
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-16 10:06:24 +0800

    Add config (3/3)

--- src/main/java/stfu/mixin/UtilMixin.java
+++ src/main/java/stfu/mixin/UtilMixin.java
@@ -5,12 +5,13 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import stfu.Config;
 
 @Mixin(Util.class)
 public abstract class UtilMixin {
     @WrapOperation(method = {"method_27956", "method_28123"}, at = @At(value = "INVOKE", target = "Ljava/lang/Thread;setName(Ljava/lang/String;)V"))
     private static void wrapUncaughtExceptionHandler(Thread instance, String name, Operation<Void> original) {
         original.call(instance, name);
-        instance.setPriority(1);//IO & Misc async
+        instance.setPriority(Config.HANDLER.instance().ioThreadPriority); // IO & Misc async
     }
 }
