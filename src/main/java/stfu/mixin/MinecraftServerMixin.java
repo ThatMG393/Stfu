d3e5269b49e8e03af5f2b7a843c14de7175f1c15
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-16 10:05:05 +0800

    Add config (2/3)

--- src/main/java/stfu/mixin/MinecraftServerMixin.java
+++ src/main/java/stfu/mixin/MinecraftServerMixin.java
@@ -5,12 +5,13 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import stfu.Config;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
     @WrapOperation(method = "startServer", at = @At(value = "INVOKE", target = "Ljava/lang/Thread;setUncaughtExceptionHandler(Ljava/lang/Thread$UncaughtExceptionHandler;)V"))
     private static void startServer(Thread instance, Thread.UncaughtExceptionHandler ueh, Operation<Void> original) {
         original.call(instance, ueh);
-        instance.setPriority(5);//Integrated Server (overridden to 8 if 4+ cores)
+        instance.setPriority(Config.HANDLER.instance().serverThreadPriority); // Integrated Server
     }
 }
